bdc9c6a2e0b1f52811ce5a1e689fef72c1b151f9
Vishal <vishalmkb@gmail.com>
2019-12-13 13:55:57 +0000

    seperated blog and observer

--- blog.py
+++ blog.py
@@ -1,22 +1,4 @@
-class Subscriber:
-    def __init__(self, name):
-        self.name = name
-    def update(self, message):
-        print('{} got message "{}"'.format(self.name, message))
-        
-class Publisher:
-    def __init__(self):
-        self.subscribers = set()
-    def register(self, who):
-        self.subscribers.add(who)
-    def unregister(self, who):
-        self.subscribers.discard(who)
-    def dispatch(self, message):
-        for subscriber in self.subscribers:
-            subscriber.update(message)
-
-
-# from observer1 import Publisher, Subscriber
+from observer import Publisher, Subscriber
 publishers = {}
 subscribers = {}
 while(True):
