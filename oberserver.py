d29e9f162dd77126b98d4627c39ed1f30efa7be8
Vishal <vishalmkb@gmail.com>
2019-12-13 13:51:25 +0000

    added observer.py

---
+++ oberserver.py
@@ -0,0 +1,66 @@
+class Subscriber:
+    def __init__(self, name):
+        self.name = name
+    def update(self, message):
+        print('{} got message "{}"'.format(self.name, message))
+        
+class Publisher:
+    def __init__(self):
+        self.subscribers = set()
+    def register(self, who):
+        self.subscribers.add(who)
+    def unregister(self, who):
+        self.subscribers.discard(who)
+    def dispatch(self, message):
+        for subscriber in self.subscribers:
+            subscriber.update(message)
+
+
+# from observer1 import Publisher, Subscriber
+publishers = {}
+subscribers = {}
+while(True):
+
+	element_type = int(input("\n\n1. Create a publisher \n2. Create a subscriber \n3. Update content in publisher \n4. Register a subscriber to publisher \n5. Unregister a subscriber\n"))
+		
+	if(element_type == 1):
+		name = input("Enter the publisher name : ")
+		name_cpy = name
+		try:
+			name = Publisher()
+			publishers[name_cpy] = name
+			#print(publishers)
+			print("Publisher "+name_cpy+" successfully created")
+		except:
+			print("Publisher "+name_cpy+" couldn't be created")
+
+	elif(element_type == 2):
+		name = input("Enter the subscriber name : ")
+		name_cpy = name
+		try:
+			name = Subscriber(name)
+			subscribers[name_cpy] = name
+			#print(subscribers)
+			print("Subscriber "+name_cpy+" successfully created")
+		except:
+			print("Subscriber "+name_cpy+" couldn't be created")
+
+	elif(element_type == 3):
+		pub_name = input("Enter the publisher's name : ")
+		val = input("Enter the content : ")
+		publishers[pub_name].dispatch(val)
+
+
+	elif(element_type == 4):
+		subs_name = input("Enter the subscriber's name : ")
+		pub_name = input("Enter the publisher's name : ")
+		#print(publishers)
+		#print(subscribers)
+		publishers[pub_name].register(subscribers[subs_name])
+
+	elif(element_type == 5):
+		subs_name = input("Enter the subscriber's name : ")
+		pub_name = input("Enter the publisher's name : ")
+		publishers[pub_name].unregister(subscribers[subs_name])
+
+
