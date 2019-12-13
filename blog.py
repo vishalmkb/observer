eb4968844ed69e5155f9bb88c936ad63bc7a8e6b
Yashas ND <yashasbharadwaj111@gmail.com>
2019-12-13 21:52:12 +0530

    Renamed conccrete classes, added main()

--- blog.py
+++ blog.py
@@ -1,6 +1,6 @@
 from observer import Subject, Subscriber
 
-class Subscriber(Subscriber):
+class blogSubscriber(Subscriber):
 	def __init__(self, name):
 		super().__init__(name)
 
@@ -11,61 +11,65 @@ class Subscriber(Subscriber):
 Subjects = {}
 Subscribers = {}
 
-while(True):
+def main():
+	while(True):
 
-	element_type = int(input("\n\n1. Create a Blog \n2. Create a subscriber \n3. Register a subscriber to Subject \n4. Unregister a subscriber \n5. Update content in Blog \n"))
-		
-	if(element_type == 1):
-		name = input("Enter the Blog name : ")
-		name_cpy = name
-		try:
-			name = Subject(name)
-			Subjects[name_cpy] = name
+		element_type = int(input("\n\n1. Create a Blog \n2. Create a subscriber \n3. Register a subscriber to Subject \n4. Unregister a subscriber \n5. Update content in Blog \n"))
+			
+		if(element_type == 1):
+			name = input("Enter the Blog name : ")
+			name_cpy = name
+			try:
+				name = Subject(name)
+				Subjects[name_cpy] = name
+				#print(Subjects)
+				print("[CREATED] Blog "+name_cpy+" successfully created")
+			except Exception as e:
+				print(e)
+				print("[ERROR] Blog "+name_cpy+" couldn't be created")
+
+		elif(element_type == 2):
+			name = input("Enter the subscriber name : ")
+			name_cpy = name
+			try:
+				name = blogSubscriber(name)
+				Subscribers[name_cpy] = name
+				#print(Subscribers)
+				print("[CREATED] Subscriber "+name_cpy+" successfully created")
+			except:
+				print("[ERROR] Subscriber "+name_cpy+" couldn't be created")
+
+
+		elif(element_type == 3):
+			subs_name = input("Enter the subscriber's name : ")
+			pub_name = input("Enter the Blog's name : ")
 			#print(Subjects)
-			print("[CREATED] Blog "+name_cpy+" successfully created")
-		except Exception as e:
-			print(e)
-			print("[ERROR] Blog "+name_cpy+" couldn't be created")
-
-	elif(element_type == 2):
-		name = input("Enter the subscriber name : ")
-		name_cpy = name
-		try:
-			name = Subscriber(name)
-			Subscribers[name_cpy] = name
 			#print(Subscribers)
-			print("[CREATED] Subscriber "+name_cpy+" successfully created")
-		except:
-			print("[ERROR] Subscriber "+name_cpy+" couldn't be created")
+			try:
+				Subjects[pub_name].register(Subscribers[subs_name])
+				print("[SUCCESS] Subscriber "+subs_name+" successfully registered to "+pub_name);
+			except:
+				print("[ERROR] Registration failed")
 
 
-	elif(element_type == 3):
-		subs_name = input("Enter the subscriber's name : ")
-		pub_name = input("Enter the Blog's name : ")
-		#print(Subjects)
-		#print(Subscribers)
-		try:
-			Subjects[pub_name].register(Subscribers[subs_name])
-			print("[SUCCESS] Subscriber "+subs_name+" successfully registered to "+pub_name);
-		except:
-			print("[ERROR] Registration failed")
+		elif(element_type == 4):
+			subs_name = input("Enter the subscriber's name : ")
+			pub_name = input("Enter the Blog's name : ")
+			try:
+				Subjects[pub_name].unregister(Subscribers[subs_name])
+				print("[SUCCESS] Subscriber "+subs_name+" successfully unregistered to "+pub_name);
+			except:
+				print("[ERROR] Unregister failed")
+
+		elif(element_type == 5):
+			pub_name = input("Enter the Blog's name : ")
+			val = input("Enter the content : ")
+			try:
+				print("[SUCCESS] Content updated");
+				Subjects[pub_name].update(val)
+			except:
+				print("[ERROR] Couldn't update content")
 
 
-	elif(element_type == 4):
-		subs_name = input("Enter the subscriber's name : ")
-		pub_name = input("Enter the Blog's name : ")
-		try:
-			Subjects[pub_name].unregister(Subscribers[subs_name])
-			print("[SUCCESS] Subscriber "+subs_name+" successfully unregistered to "+pub_name);
-		except:
-			print("[ERROR] Unregister failed")
-
-	elif(element_type == 5):
-		pub_name = input("Enter the Blog's name : ")
-		val = input("Enter the content : ")
-		try:
-			print("[SUCCESS] Content updated");
-			Subjects[pub_name].update(val)
-		except:
-			print("[ERROR] Couldn't update content")
-
+if (__name__ == "__main__"):
+	main()
--- group.py
+++ group.py
@@ -1,6 +1,6 @@
 from observer import Subject, Subscriber, Publisher
 
-class Subscriber(Subscriber):
+class groupSubscriber(Subscriber):
 	def __init__(self, name):
 		super().__init__(name)
 		self.publisher = Publisher(name)
@@ -16,61 +16,65 @@ class Subscriber(Subscriber):
 Subjects = {}
 Subscribers = {}
 
-while(True):
+def main():
+	while(True):
 
-	element_type = int(input("\n\n1. Create a group \n2. Create a user \n3. Register a user to group \n4. Unregister a user to group\n5. Send message in group \n"))
-		
-	if(element_type == 1):
-		name = input("Enter the group name : ")
-		name_cpy = name
-		try:
-			name = Subject(name)
-			Subjects[name_cpy] = name
+		element_type = int(input("\n\n1. Create a group \n2. Create a user \n3. Register a user to group \n4. Unregister a user to group\n5. Send message in group \n"))
+			
+		if(element_type == 1):
+			name = input("Enter the group name : ")
+			name_cpy = name
+			try:
+				name = Subject(name)
+				Subjects[name_cpy] = name
+				#print(Subjects)
+				print("[CREATED] Subject "+name_cpy+" successfully created")
+			except:
+				print("[ERROR] Subject "+name_cpy+" couldn't be created")
+
+		elif(element_type == 2):
+			name = input("Enter the User name : ")
+			name_cpy = name
+			try:
+				name = groupSubscriber(name)
+				Subscribers[name_cpy] = name
+				#print(Subscribers)
+				print("[CREATED] User "+name_cpy+" successfully created")
+			except Exception as e:
+				print(e)
+				print("[ERROR] User "+name_cpy+" couldn't be created")
+
+
+		elif(element_type == 3):
+			subs_name = input("Enter the User's name : ")
+			pub_name = input("Enter the Group's name : ")
 			#print(Subjects)
-			print("[CREATED] Subject "+name_cpy+" successfully created")
-		except:
-			print("[ERROR] Subject "+name_cpy+" couldn't be created")
-
-	elif(element_type == 2):
-		name = input("Enter the User name : ")
-		name_cpy = name
-		try:
-			name = Subscriber(name)
-			Subscribers[name_cpy] = name
 			#print(Subscribers)
-			print("[CREATED] User "+name_cpy+" successfully created")
-		except Exception as e:
-			print(e)
-			print("[ERROR] User "+name_cpy+" couldn't be created")
+			try:
+				Subjects[pub_name].register(Subscribers[subs_name])
+				print("[SUCCESS] User "+subs_name+" successfully registered to "+pub_name);
+			except:
+				print("[ERROR] Registration failed")
 
+		elif(element_type == 4):
+			subs_name = input("Enter the User's name : ")
+			pub_name = input("Enter the Group's name : ")
+			try:
+				Subjects[pub_name].unregister(Subscribers[subs_name])
+				print("[SUCCESS] User "+subs_name+" successfully unregistered to "+pub_name);
+			except:
+				print("[ERROR] Unregister failed")
 
-	elif(element_type == 3):
-		subs_name = input("Enter the User's name : ")
-		pub_name = input("Enter the Group's name : ")
-		#print(Subjects)
-		#print(Subscribers)
-		try:
-			Subjects[pub_name].register(Subscribers[subs_name])
-			print("[SUCCESS] User "+subs_name+" successfully registered to "+pub_name);
-		except:
-			print("[ERROR] Registration failed")
+		elif(element_type == 5):
+			user_name = input("Enter the User's name : ")
+			grp_name = input("Enter the Group's name : ")
+			message = input("Enter the content : ")
+			val = {'message':message, 'group':grp_name}
+			try:
+				Subscribers[user_name].publish(Subjects[grp_name], val)
+				print("[SUCESS] User "+user_name+" sent the message successfully")
+			except:
+				print("[ERROR] Failed to send message")
 
-	elif(element_type == 4):
-		subs_name = input("Enter the User's name : ")
-		pub_name = input("Enter the Group's name : ")
-		try:
-			Subjects[pub_name].unregister(Subscribers[subs_name])
-			print("[SUCCESS] User "+subs_name+" successfully unregistered to "+pub_name);
-		except:
-			print("[ERROR] Unregister failed")
-
-	elif(element_type == 5):
-		user_name = input("Enter the User's name : ")
-		grp_name = input("Enter the Group's name : ")
-		message = input("Enter the content : ")
-		val = {'message':message, 'group':grp_name}
-		try:
-			Subscribers[user_name].publish(Subjects[grp_name], val)
-			print("[SUCESS] User "+user_name+" sent the message successfully")
-		except:
-			print("[ERROR] Failed to send message")
+if (__name__ == "__main__"):
+	main()
